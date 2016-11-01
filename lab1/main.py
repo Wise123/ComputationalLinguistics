10c96705dd20897db38d01d34f576b99d770d06c
Wise <roman_nagaev_1993@mail.ru>
2016-11-01 10:25:05 +0600

    убрал точки с запятыми

--- lab1/main.py
+++ lab1/main.py
@@ -12,12 +12,12 @@ def getSoupByUrl(url):
 
 dnsHomeUrl = 'http://www.dns-shop.ru' #начинаем с домашней страницы потому что при переходе к ноутбукам сайт даёт нам айдишник
 
-mainPage = getSoupByUrl(dnsHomeUrl);#получаем суп главной страницы
+mainPage = getSoupByUrl(dnsHomeUrl)#получаем суп главной страницы
 menuCatalog = mainPage.find_all(id="menu-catalog")[0]# получаем меню-каталог
-laptopsAndTablets = menuCatalog.find_all('div', class_="sub-wrap")[0].findAll('a')[0];#получаем элемент-ссылку на страницу с ноутбуками
+laptopsAndTablets = menuCatalog.find_all('div', class_="sub-wrap")[0].findAll('a')[0]#получаем элемент-ссылку на страницу с ноутбуками
 
 # я не знаю как выяснить общее количество страниц, пока оставим это пользователю
-pagesNumber = input('сколько страниц вы хотите загрузить?');
+pagesNumber = input('сколько страниц вы хотите загрузить?')
 # print(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber)
 # получаем суп страницы с ноутбуками
 laptopsPage = getSoupByUrl(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber)
