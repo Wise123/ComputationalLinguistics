d907e6578a67708e9d66b187256d8d0ed55e81e9
Wise <roman_nagaev_1993@mail.ru>
2016-11-10 16:58:43 +0600

    промедуточный вариант

--- lab1/main.py
+++ lab1/main.py
@@ -7,6 +7,7 @@ def getSoupByUrl(url):
     request = urllib.request.Request(dnsHomeUrl)
     opener = urllib.request.build_opener()
     response = opener.open(request)
+    html_doc = urllib.request.urlopen(dnsHomeUrl).read()
     return BeautifulSoup(response.read())
 
 
@@ -18,8 +19,26 @@ laptopsAndTablets = menuCatalog.find_all('div', class_="sub-wrap")[0].findAll('a
 
 # я не знаю как выяснить общее количество страниц, пока оставим это пользователю
 pagesNumber = input('сколько страниц вы хотите загрузить?')
-# print(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber)
+print(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber)
 # получаем суп страницы с ноутбуками
 laptopsPage = getSoupByUrl(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber)
 
-print(laptopsPage.prettify())
+#print(laptopsPage.prettify())1
+
+html_doc = urllib.request.urlopen(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber).read()
+soup = BeautifulSoup(html_doc)
+opisanie = soup.find_all('div',class_="price_g")
+opisanie1 = soup.find_all('div',class_="item-name")
+opisanie2 = soup.find_all('div',class_="item-desc")
+i=0
+k=0
+j=0
+while i<len(opisanie):
+    print (opisanie[i].get_text())
+    i=i+1
+while j<len(opisanie1):
+    print (opisanie1[j].get_text())
+    j=j+1
+while k<len(opisanie2):
+    print (opisanie2[k].get_text())
+    k=k+1
--- parseDNS/parseDNS/parseDNS.py
+++ parseDNS/parseDNS/parseDNS.py
@@ -1,21 +1,17 @@
-from urllib2 import urlopen
-from bs4 import BeautifulSoup
-import re
-html_doc = urlopen('http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/').read()
+html_doc = urllib.request.urlopen(dnsHomeUrl + laptopsAndTablets['href'] + '?p=' + pagesNumber).read()
 soup = BeautifulSoup(html_doc)
-collect = soup.find('div', 'catalog-category')
-opisanie = collect.findAll('div','price_g')
-opisanie1 = collect.findAll('div','item-name')
-opisanie2 = collect.findAll('div','item-desc')
+opisanie = soup.find_all('div',class_="price_g")
+opisanie1 = soup.find_all('div',class_="item-name")
+opisanie2 = soup.find_all('div',class_="item-desc")
 i=0
 k=0
 j=0
 while i<len(opisanie):
-    print opisanie[i].get_text()
+    print (opisanie[i].get_text())
     i=i+1
 while j<len(opisanie1):
-    print opisanie1[j].get_text()
+    print (opisanie1[j].get_text())
     j=j+1
 while k<len(opisanie2):
-    print opisanie2[k].get_text()
+    print (opisanie2[k].get_text())
     k=k+1
